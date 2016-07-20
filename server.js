3fc71e4faec822575395302d1e3c640f265120e3
raimonfuns <raimonfuns@163.com>
2016-07-20 10:46:50 +0800

    init

---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict'
+
+process.env.VUE_ENV = 'server'
+
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const favicon = require('serve-favicon')
+const serialize = require('serialize-javascript')
+const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
+
+const renderer = createBundleRenderer(fs.readFileSync('./dist/server-bundle.js', 'utf-8'), {
+  cache: require('lru-cache')({ max: 10000 })
+})
+
+const stats = []
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, 'dist')))
+app.use(favicon(path.resolve(__dirname, 'dist/logo.png')))
+
+app.get('*', (req, res) => {
+  const start = Date.now()
+  const context = { url: req.url }
+  const renderStream = renderer.renderToStream(context)
+  let firstChunk = true
+
+  res.write('<!DOCTYPE html><body>')
+
+  // 有data说明fetchServerData已完成?
+  renderStream.on('data', chunk => {
+    if (firstChunk) {
+      // send down initial store state
+      if (context.initialState) {
+        res.write(`<script>window.__INITIAL_STATE__=${
+          serialize(context.initialState, { isJSON: true })
+        }</script>`)
+      }
+      firstChunk = false
+    }
+    res.write(chunk)
+  })
+
+  renderStream.on('end', () => {
+    res.end(`<script src="/client-bundle.js"></script></body>`)
+    const used = Date.now() - start
+    stats.push(used)
+    console.log(`request used: ${(Date.now() - start)}ms`)
+    console.log(`average: ${(stats.reduce((s, t) => s + t, 0) / stats.length).toFixed(2)}ms`)
+  })
+
+  renderStream.on('error', err => {
+    throw err
+  })
+})
+
+app.listen(3000, () => {
+  console.log('ready at localhost:3000')
+})
